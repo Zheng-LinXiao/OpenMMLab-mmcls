98046554e8383f0df7cc5d3364b8890fb6ab9686
LinXiaoZheng <90811472+Zheng-LinXiao@users.noreply.github.com>
2023-02-07 23:42:40 +0800

    Add files via upload

---
+++ ResNet-18-flower.py
@@ -0,0 +1,79 @@
+# model settings
+model = dict(
+    type='ImageClassifier',
+    backbone=dict(
+        type='ResNet',
+        depth=18,
+        num_stages=4,
+        out_indices=(3, ),
+        style='pytorch'),
+    neck=dict(type='GlobalAveragePooling'),
+    head=dict(
+        type='LinearClsHead',
+        num_classes=1000,
+        in_channels=512,
+        loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+        topk=(1, 5),
+    ))
+# dataset settings
+dataset_type = 'CustomDataset'
+classes = ['daisy', 'dandelion', 'rose','sunflower','tulip']
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='RandomResizedCrop', size=224),
+    dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='ImageToTensor', keys=['img']),
+    dict(type='ToTensor', keys=['gt_label']),
+    dict(type='Collect', keys=['img', 'gt_label'])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', size=(256, -1)),
+    dict(type='CenterCrop', crop_size=224),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='ImageToTensor', keys=['img']),
+    dict(type='Collect', keys=['img'])
+]
+data = dict(
+    samples_per_gpu=128,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        data_prefix='D:/code/mmclassification-master/data/train',
+        ann_file='D:/code/mmclassification-master/data/train.txt',
+        classes=classes,
+        pipeline=train_pipeline
+    ),
+    val=dict(
+        type=dataset_type,
+        data_prefix='D:/code/mmclassification-master/data/val',
+        ann_file='D:/code/mmclassification-master/data/val.txt',
+        classes=classes,
+        pipeline=test_pipeline
+    ))
+evaluation = dict(interval=5, metric='accuracy')
+# optimizer
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(policy='step', step=[30, 60, 90])
+runner = dict(type='EpochBasedRunner', max_epochs=100)
+# checkpoint saving
+checkpoint_config = dict(interval=1)
+# yapf:disable
+log_config = dict(
+    interval=100,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        # dict(type='TensorboardLoggerHook')
+    ])
+# yapf:enable
+
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = 'D:/code/mmclassification-master/checkpoints/resnet-18.pth'
+resume_from = None
+workflow = [('train', 1)]
